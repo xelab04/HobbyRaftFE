2c37c38cb5d457247c022eddd9cea8ee62d267ab
xelab04 <alexbissessur@gmail.com>
2024-08-03 19:28:43 +0400

    Initial Commit

---
+++ HobbyRaft/nuxt.config.ts
@@ -0,0 +1,27 @@
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: ['nuxtjs-naive-ui'],
+  vite: {
+    plugins: [
+      AutoImport({
+        imports: [
+          {
+            'naive-ui': [
+              'useDialog',
+              'useMessage',
+              'useNotification',
+              'useLoadingBar'
+            ]
+          }
+        ]
+      }),
+      Components({
+        resolvers: [NaiveUiResolver()]
+      })
+    ]
+  }
+})
